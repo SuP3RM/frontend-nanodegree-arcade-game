20ea04ca960f484b09935e7435bad63f241ae745
Mario Aranda <mario.aranda2551@gmail.com>
2018-08-07 21:17:02 -0500

    Last Commit
    Added Credit to references, beautify code, and kinda fix replay button.

--- js/app.js
+++ js/app.js
@@ -3,62 +3,61 @@
 
 // Enemies our player must avoid
 var Enemy = function(x, y, speed) {
-    this.x = x;
-    this.y = y + 55;
-    this.speed = speed;
+  this.x = x;
+  this.y = y + 55;
+  this.speed = speed;
 
-    this.step = 101;
-    this.boundary = this.step * 5;
-    this.resetPos = -this.step;
+  this.step = 101;
+  this.boundary = this.step * 5;
+  this.resetPos = -this.step;
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+  // The image/sprite for our enemies, this uses
+  // a helper we've provided to easily load images
+  this.sprite = 'images/enemy-bug.png';
 };
 
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+  // You should multiply any movement by the dt parameter
+  // which will ensure the game runs at the same speed for
+  // all computers.
 
-    // if enemy is not passed boundary
-    if (this.x < this.boundary) {
-      // increment x by speed * dt
-      // move forward
-      this.x += this.speed * dt;
-    }
-    else {
+  // if enemy is not passed boundary
+  if (this.x < this.boundary) {
+    // increment x by speed * dt
+    // move forward
+    this.x += this.speed * dt;
+  } else {
     this.x = this.resetPos;
-    }
+  }
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Hero class
-  // Constrctor
-    // properties
-      // x position
-      // y position
-      // sprite
+// Constrctor
+// properties
+// x position
+// y position
+// sprite
 
-    //methods
-      // update position
-        // check collision here
-          // did player x and y collide with enemy?
-          // check win here?
-          // did player x and y reach final title?
-      // Render
-        // Draw player sprite on current x and y coord position
-      // handle keyboard handleInput
-        // update player's x and y property according to input
-      // Reset hero
-        // set x and y to starting x and y
+//methods
+// update position
+// check collision here
+// did player x and y collide with enemy?
+// check win here?
+// did player x and y reach final title?
+// Render
+// Draw player sprite on current x and y coord position
+// handle keyboard handleInput
+// update player's x and y property according to input
+// Reset hero
+// set x and y to starting x and y
 
 class Hero {
   constructor() {
@@ -80,14 +79,14 @@ class Hero {
   }
 
   handleInput(input) {
-    switch(input) {
+    switch (input) {
       case 'left':
-      if (this.x > 0) {
-        this.x -= this.step;
-      }
+        if (this.x > 0) {
+          this.x -= this.step;
+        }
         break;
       case 'right':
-        if (this.x < 404 ) {
+        if (this.x < 404) {
           this.x += this.step;
         }
         break;
@@ -98,7 +97,7 @@ class Hero {
         break;
       case 'down':
         if (this.y < 380)
-        this.y += this.jump;
+          this.y += this.jump;
         break;
     }
   }
@@ -106,12 +105,12 @@ class Hero {
   update() {
     // check for collisions here
     for (let enemy of allEnemies) {
-        if (this.y === enemy.y && (enemy.x + enemy.step/1.5 > this.x && enemy.x < this.x + this.step/1.5) ) {
-          this.reset();
-        }
+      if (this.y === enemy.y && (enemy.x + enemy.step / 1.5 > this.x && enemy.x < this.x + this.step / 1.5)) {
+        this.reset();
+      }
     }
-    // check win here?
-    if(this.y === 55) {
+    // check win here
+    if (this.y === 55) {
       this.winning = true;
     }
   }
@@ -130,25 +129,30 @@ class Hero {
 const player = new Hero();
 const bug1 = new Enemy(-101, 0, 200);
 const bug2 = new Enemy(-101, 83, 300);
-const bug3 = new Enemy((-101 * 2.5), 83, 300);
+const bug3 = new Enemy((-101 * 2.5), 83 * 2, 350);
+const bug4 = new Enemy((-101 * 2.5), 83 * 3, 125);
+const bug5 = new Enemy((-101 * 2.5), 83 * 3, 250);
 
 let allEnemies = [];
-allEnemies.push(bug1, bug2, bug3);
+allEnemies.push(bug1, bug2, bug3, bug4, bug5);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
+  var allowedKeys = {
+    37: 'left',
+    38: 'up',
+    39: 'right',
+    40: 'down'
+  };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+  player.handleInput(allowedKeys[e.keyCode]);
 });
 
-//clock/timer
+
+// Credit to Ryan Waite for the clock/timer, used from last project:
+// https://github.com/ryanwaite28/script-store/blob/master/js/stop-watch.js
+// clock/timer
 let StopWatch = function StopWatch() {
   const self = this;
 
